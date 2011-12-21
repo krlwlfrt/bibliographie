f410acfa60b267d73ed8a8df53ff7f09cf099e1c
Karl-Philipp Wulfert <animungo@gmail.com>
2011-12-21 02:45:01 +0100

    Remove nonsens notification

--- _footer.php
+++ _footer.php
@@ -5,11 +5,7 @@
 
 			<div id="footer">
 				<strong>user</strong>: <?php echo $_SERVER['PHP_AUTH_USER']?>,
-				<strong>duration</strong>: <?php echo round(microtime(true) - BIBLIOGRAPHIE_SCRIPT_START, 6)?>s<?php
-if(!DB::status())
-	echo ', <strong>cached content only</strong>';
-?>
-
+				<strong>duration</strong>: <?php echo round(microtime(true) - BIBLIOGRAPHIE_SCRIPT_START, 6)?>s
 			</div>
 		</div>
 
